c3b904fb481a65f324f1859879861783747bb59f
EggFine <1079742633@qq.com>
2025-06-07 20:16:57 +0800

    初始化项目
    第一次上传

---
+++ src/main/java/com/blbilink/neoLogin/NeoLogin.java
@@ -0,0 +1,17 @@
+package com.blbilink.neoLogin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NeoLogin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
