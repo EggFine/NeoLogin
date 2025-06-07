c043c36e1798491063669f44858f310ab704c77b
EggFine <1079742633@qq.com>
2025-06-08 01:06:09 +0800

    更新
    更新了Logo
    修复了build.gradle
    依赖了NeoLibrary

--- src/main/java/com/blbilink/neoLogin/NeoLogin.java
+++ src/main/java/com/blbilink/neoLogin/NeoLogin.java
@@ -1,9 +1,11 @@
 package com.blbilink.neoLogin;
 
+import com.blbilink.neoLibrary.utils.I18n;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NeoLogin extends JavaPlugin {
 
+    public I18n i18n;
     @Override
     public void onEnable() {
         // Plugin startup logic
