a7326d08b1e9aa03b2bd0f79ffe4ad8ab1dbb964
EggFine <1079742633@qq.com>
2025-06-08 03:48:52 +0800

    update
    引入NeoLibrary
    添加FoliaUtil的使用
    增加启动Logo
    增加bStats统计

--- src/main/java/com/blbilink/neoLogin/NeoLogin.java
+++ src/main/java/com/blbilink/neoLogin/NeoLogin.java
@@ -1,19 +1,27 @@
 package com.blbilink.neoLogin;
 
+import com.blbilink.neoLibrary.utils.FoliaUtil;
 import com.blbilink.neoLibrary.utils.I18n;
+import com.blbilink.neoLibrary.utils.Metrics;
+import com.blbilink.neoLibrary.utils.TextUtil;
+
+import java.util.Collections;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NeoLogin extends JavaPlugin {
 
-    public I18n i18n;
+    public FoliaUtil foliaUtil;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().info(TextUtil.getLogo("Loading...", "NeoLogin", "The Next Generation blbiLogin\nSpigotMC: https://www.spigotmc.org/resources/125813/", this, Collections.singletonList("EggFine"), null));
+        Metrics metrics = new Metrics(this, 26113);
+        foliaUtil = new FoliaUtil(this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        
     }
 }
